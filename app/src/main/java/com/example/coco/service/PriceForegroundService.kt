3aed017542bcc4a1140bb3f965c63922729fe16e
sphy1597 <sphy1597@naver.com>
2024-02-01 18:27:15 +0900

    feat: foreGroundService 구현

--- app/src/main/java/com/example/coco/service/PriceForegroundService.kt
+++ app/src/main/java/com/example/coco/service/PriceForegroundService.kt
@@ -1,17 +1,29 @@
 package com.example.coco.service
 
-import android.app.Notification
-import android.app.PendingIntent
-import android.app.Service
+import android.app.*
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.example.coco.R
+import com.example.coco.dataModel.CurrentPrice
+import com.example.coco.dataModel.CurrentPriceResult
+import com.example.coco.repository.NetWorkRepository
 import com.example.coco.view.main.MainActivity
+import com.google.gson.Gson
+import kotlinx.coroutines.*
 import timber.log.Timber
+import java.util.*
+import kotlin.collections.ArrayList
+
 
 // 앱 종료시에도 사용 가능한 서비스
 class PriceForegroundService : Service() {
 
+	private val netWorkRepo = NetWorkRepository()
 	private val NOTIFICATION_ID = 10000
+	lateinit var job : Job
 
 	override fun onCreate() {
 		super.onCreate()
@@ -19,13 +31,33 @@ class PriceForegroundService : Service() {
 
 	override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
-		when(intent?.action){
+		when (intent?.action) {
+			// Todo : 2번 눌렀을 때 대처해야됨
 			"START" -> {
-				Timber.tag("테스트").d("START")
+
+				job = CoroutineScope(Dispatchers.Default).launch {
+
+					while (true){
+						Timber.tag("테스트").d("START")
+						startForeground(NOTIFICATION_ID, makeNotification())
+
+						delay(3000)
+					}
+
+				}
+
 			}
 
 			"STOP" -> {
 				Timber.tag("테스트").d("STOP")
+				try{
+					job.cancel()
+					stopForeground(STOP_FOREGROUND_REMOVE)
+					stopSelf()
+				}catch(e: java.lang.Exception){
+
+				}
+
 			}
 
 		}
@@ -38,20 +70,73 @@ class PriceForegroundService : Service() {
 		return null
 	}
 
-	suspend fun makeNotification() : Notification {
+	suspend fun makeNotification(): Notification {
+
+		val result = getAllCoinList()
+
+		val randomNum = Random().nextInt(result.size)
+
+		val title = result[randomNum].coinName
+		val content = result[randomNum].coinInfo.fluctate_24H
+
 		val intent = Intent(this, MainActivity::class.java).apply {
 			flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
 		}
 
-		val pendingIntent : PendingIntent = PendingIntent.getActivity(
+		val pendingIntent: PendingIntent = PendingIntent.getActivity(
 			this,
 			0,
 			intent,
 			PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
 		)
 
+		val builder = NotificationCompat.Builder(this, "CHANNEL_ID")
+			.setSmallIcon(R.drawable.ic_baseline_access_alarms_24)
+			.setContentTitle("코인이름 : ${title}")
+			.setContentText("변동가격 : ${content}")
+			.setContentIntent(pendingIntent)
+			.setPriority(NotificationCompat.PRIORITY_DEFAULT)
+			.setCategory(NotificationCompat.CATEGORY_MESSAGE)
 
-		return
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+			val name = "name"
+			val descriptionText = "descriptionText"
+			val importance = NotificationManager.IMPORTANCE_LOW
+			val channel = NotificationChannel("CHANNEL_ID", name, importance).apply {
+				description = descriptionText
+			}
+
+			val notificationManager: NotificationManager =
+				getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+			notificationManager.createNotificationChannel(channel)
+
+
+		}
+		return builder.build()
+	}
+
+	suspend fun getAllCoinList(): ArrayList<CurrentPriceResult> {
+		val result = netWorkRepo.getCurrentCoinList()
+
+		val currentPriceResultList = ArrayList<CurrentPriceResult>()
+
+		for (coin in result.data) {
+
+			try {
+				val gson = Gson()
+				val gsonToJSon = gson.toJson(result.data.get(coin.key))
+				val gsonFromJson = gson.fromJson(gsonToJSon, CurrentPrice::class.java)
+
+				val currentPriceResult = CurrentPriceResult(coin.key, gsonFromJson)
+				currentPriceResultList.add(currentPriceResult)
+
+			} catch (e: java.lang.Exception) {
+				Timber.d(e.toString())
+			}
+
+		}
+
+		return currentPriceResultList
 	}
 
 
--- app/src/main/java/com/example/coco/view/setting/SettingActivity.kt
+++ app/src/main/java/com/example/coco/view/setting/SettingActivity.kt
@@ -1,17 +1,28 @@
 package com.example.coco.view.setting
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.example.coco.databinding.ActivityMainBinding
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.example.coco.dataModel.CurrentPrice
+import com.example.coco.dataModel.CurrentPriceResult
 import com.example.coco.databinding.ActivitySettingBinding
+import com.example.coco.repository.NetWorkRepository
 import com.example.coco.service.PriceForegroundService
+import com.google.gson.Gson
+import timber.log.Timber
 
 
 class SettingActivity : AppCompatActivity() {
 
-	private lateinit var binding : ActivitySettingBinding
+	private lateinit var binding: ActivitySettingBinding
+
+
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -19,27 +30,55 @@ class SettingActivity : AppCompatActivity() {
 		val view = binding.root
 		setContentView(view)
 
-		binding.startForeground.setOnClickListener {
-			Toast.makeText(this, "Start", Toast.LENGTH_LONG).show()
 
-			val intent = Intent(this, PriceForegroundService::class.java)
-			intent.action = "START"
-			startService(intent)
+		val isTiramisuOrHigher = Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
+		val notificationPermission = Manifest.permission.POST_NOTIFICATIONS
+
+		var hasNotificationPermission =
+			if (isTiramisuOrHigher)
+				ContextCompat.checkSelfPermission(
+					this,
+					notificationPermission
+				) == PackageManager.PERMISSION_GRANTED
+			else true
+
+		val launcher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+			hasNotificationPermission = it
+			if (it) {
+				val intent = Intent(this, PriceForegroundService::class.java)
+				intent.action = "START"
+				startService(intent)
+			}
+		}
+
+		binding.startForeground.setOnClickListener {
+
+			Toast.makeText(this, "start", Toast.LENGTH_LONG).show()
+
+			if (hasNotificationPermission) {
+				val intent = Intent(this, PriceForegroundService::class.java)
+				intent.action = "START"
+				startService(intent)
+			} else if (isTiramisuOrHigher) {
+				launcher.launch(notificationPermission)
+			}
 
 
 		}
 
 		binding.stopForeground.setOnClickListener {
-			Toast.makeText(this, "Stop", Toast.LENGTH_LONG).show()
+
+			Toast.makeText(this, "stop", Toast.LENGTH_LONG).show()
+
 			val intent = Intent(this, PriceForegroundService::class.java)
 			intent.action = "STOP"
 			startService(intent)
 
+
 		}
 
 
-
-
-
 	}
+
+
 }
